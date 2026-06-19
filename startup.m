267e8d4846c4a3ebb48c64f785bd7e4f68a2c25c
docNathanM <nmurray@olemiss.edu>
2026-06-19 11:29:33 -0500

    Update startup.m
    Moving repositories around and changing the way that the Matlab startup.m file is adding these to the path. I need to make sure that the git repositories are up to date separately.

--- startup.m
+++ startup.m
@@ -1,10 +1,16 @@
-% Repository Matlab_Murray_UtilitiesGeneral includes this startup.m file
-% that needs to be copied to the home directory for Matlab at
+% Repositories including Matlab_Murray_UtilitiesGeneral are available on
+% github at https://github.com/docNathanM
+% In order to make these available for use in Matlab, 
+%  (1) the repositories need to be cloned to local folders, and 
+%  (2) the local folders need to be placed on the Matlab path.
+% This script is included in Matlab_Murray_UtilitiesGeneral as an example.
+% Copy this file to the home directory for Matlab at
 % ~/Documents/MATLAB/
-% At startup, MATLAB runs the startup.m script.
+% Then, modify the file as needed based on the location of the local 
+% repositories.
+% At startup, MATLAB runs the startup.m script automatically.
 
-addpath(genpath('~/Desktop/gitRepsLocal/Matlab_Murray_ThermoUtils/'))
-addpath(genpath('~/Desktop/gitRepsLocal/Matlab_Murray_UtilitiesGeneral/'))
+addpath(genpath('/Volumes/Caleb/32 Methods/32.02 Matlab_Murray_gitRepsLocal/'))
 
 [~,name] = system('hostname');
 
